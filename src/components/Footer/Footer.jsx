09e5660c450462b0bae4dd5b53baca07df54a2d4
Kunal Pandey <pandeykunal2084@gmail.com>
2025-02-12 12:24:19 +0530

    initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./footer.css"
+function Footer() {
+  return (
+    <div className='footer' >
+      Made With ❤️ by Kunal Pandey
+    </div>
+  )
+}
+
+export default Footer
